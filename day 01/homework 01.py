ffa6b9a3f129227e6976a0dc965922a4fecb10de
vaxushti <mujiravaxo@gmail.com>
2024-02-18 00:11:29 +0400

    first commit

---
+++ day 01/homework 01.py
@@ -0,0 +1,13 @@
+name="vaxo"
+surname="mujirishvili"
+age="22 wlis"
+height="184 sm"
+weight="92 kg"
+femily="myavs meugle da bavshvi"
+
+
+
+print(name+" "+surname+" "+(age)+" "+height+ " "+weight+" "+femily)
+
+
+
---
+++ day 02/homework 02.py
@@ -0,0 +1,21 @@
+print("mogesalmebit")
+name="vaxo"
+surname="mujirishvili"
+age="22 wlis"
+print(name)
+print(surname)
+print(age)
+
+print("megobrebis dziebis processhi var ")
+
+print("chemi azrit goa adzlevs axalgazrdebs da zrdasrulebsac imis sashualebas rom ganvitardnen da tavisi cxovreba shecvalon uketesobisken")
+("megobruli garemoa racarunda moxdes yvelafershi gverdshi gidganan da amatuimsakitxshi amomwurav pasuxs gadzleven cdiloben bavshvebis ")
+("ganvitarebaze kompiuteruli tamashebis moshorebaze da  swori da jansagi cxovrebis dawyebashi gvexmarebit jer gakvetilsac ardavswrebivar")
+("magram dzalian didi motivacia makvs kompiuterze windousis gadayenebac arvici :D magram darwmunebulivar rom sul mokle droshi me shemdgar")
+("programisti viknebi mekneba chemi piradi shemosavali da ganvitardebi rogorc pirovnulad aseve cxovrebiseulad am yvelafristvis ki dzalian ")
+("did madlobas vuxdi nika keshelavas da goas mtlian shemadgenlobas mevxedav ertobas gverdshi dgomas da imasrom dgevandel covrebashi ")
+("pizikuri mushaobit araferi argamodis da adamiani ert adgilas xar gachedili verc cin midixar verc ukan goa ki cdilobs da aketebs ")
+("imas rom axalgazrdobidanve gvkondes chveni piradi shemosavali arviyot  sxvisi monebi :)    ") 
+
+("madlobas goas <3 aseti motivaciistvis da chemicxovrebis radikalurad shecvlistvis fikrebiukve shemecvala bevr sakitxtan dakavshirebit")
+("darwmunebulivar rom sulmokle drosghi mxolod fikrebikiara mec shevicvlebi radiklalurad uketesobisken da gavxdebi chamoyalibebuli adamiani")
