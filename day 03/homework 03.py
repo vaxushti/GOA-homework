ce46b11dadf8bee97991b82c197383f221ba0eb1
vaxushti <mujiravaxo@gmail.com>
2024-02-22 14:24:47 +0400

    second commit

---
+++ day 03/homework 03.py
@@ -0,0 +1,13 @@
+name = "nino eqizashvili"
+name1 = "dato mujirishvili"
+name2 = "dachi mujirishvili"
+name3 = "ani osefashvili"
+name4 = "livia mujirishvili"
+
+
+
+print(name)
+print(name1)
+print(name2)
+print(name3)
+print(name4)
---
+++ day 04/homework 04.py
@@ -0,0 +1,15 @@
+age = 38
+age1 = 45
+age2 = 20
+age3 = 23
+age4 = 1
+
+text = "my parents are"
+
+print(text + " " + str(age) + " " + str(age1) + " " + str(age2) + " " + str(age3) + " " + str(age4))
+
+text1 = "after two yars they became"
+
+
+print(text1 + " " + str(age+2) + " " + str(age1+2) + " " + str(age2+2) + " " + str(age3+2) + " " + str(age4+2))
+
